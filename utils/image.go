400211b8e3db55225803a599d0037b3537eab6b4
Jared439 <a1203598779jf@gmail.com>
2026-08-27 11:46:47 -0400

    Update image.go

--- utils/image.go
+++ utils/image.go
@@ -6,6 +6,7 @@ import (
 	"image"
 	"image/gif"
 	"image/jpeg"
+	_ "image/png"
 	"io"
 	"math"
 	"net/http"
@@ -20,6 +21,7 @@ type Image struct {
 	Object  image.Image
 	Content string
 }
+
 type Images []Image
 
 func NewImage(url string) (*Image, error) {
@@ -39,52 +41,72 @@ func NewImage(url string) (*Image, error) {
 			return nil, err
 		}
 
-		return &Image{Object: img, Content: url}, nil
+		return &Image{
+			Object:  img,
+			Content: url,
+		}, nil
 	}
 
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
-
 	defer res.Body.Close()
 
 	var img image.Image
 	suffix := strings.ToLower(path.Ext(url))
+
 	switch suffix {
 	case ".png":
 		if img, _, err = image.Decode(res.Body); err != nil {
 			return nil, err
 		}
+
 	case ".jpg", ".jpeg":
 		if img, err = jpeg.Decode(res.Body); err != nil {
 			return nil, err
 		}
-	case "webp":
+
+	case ".webp":
 		if img, err = webp.Decode(res.Body); err != nil {
 			return nil, err
 		}
-	case "gif":
-		ticks, err := gif.DecodeAll(res.Body)
-		if err != nil {
-			return nil, err
+
+	case ".gif":
+		ticks, decodeErr := gif.DecodeAll(res.Body)
+		if decodeErr != nil {
+			return nil, decodeErr
 		}
+
+		if len(ticks.Image) == 0 {
+			return nil, fmt.Errorf("GIF contains no image frames")
+		}
+
 		img = ticks.Image[0]
+
+	default:
+		return nil, fmt.Errorf("unsupported image format: %s", suffix)
 	}
 
-	return &Image{Object: img, Content: url}, nil
+	return &Image{
+		Object:  img,
+		Content: url,
+	}, nil
 }
 
 func NewImageContent(content string) *Image {
-	return &Image{Content: content}
+	return &Image{
+		Content: content,
+	}
 }
 
 func ConvertToBase64(url string) (string, error) {
 	if strings.HasPrefix(url, "data:image/") {
-		data := strings.Split(url, ",")
+		data := strings.SplitN(url, ",", 2)
 		if len(data) != 2 {
 			return "", nil
 		}
+
 		return data[1], nil
 	}
 
@@ -92,7 +114,6 @@ func ConvertToBase64(url string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	defer res.Body.Close()
 
 	data, err := io.ReadAll(res.Body)
@@ -104,14 +125,29 @@ func ConvertToBase64(url string) (string, error) {
 }
 
 func (i *Image) GetWidth() int {
-	return i.Object.Bounds().Max.X
+	if i == nil || i.Object == nil {
+		return 0
+	}
+
+	return i.Object.Bounds().Dx()
 }
 
 func (i *Image) GetHeight() int {
-	return i.Object.Bounds().Max.Y
+	if i == nil || i.Object == nil {
+		return 0
+	}
+
+	return i.Object.Bounds().Dy()
 }
 
-func (i *Image) GetPixel(x int, y int) (uint32, uint32, uint32, uint32) {
+func (i *Image) GetPixel(
+	x int,
+	y int,
+) (uint32, uint32, uint32, uint32) {
+	if i == nil || i.Object == nil {
+		return 0, 0, 0, 0
+	}
+
 	return i.Object.At(x, y).RGBA()
 }
 
@@ -121,15 +157,23 @@ func (i *Image) GetPixelColor(x int, y int) (int, int, int) {
 }
 
 func (i *Image) CountTokens(model string) int {
+	if i == nil || i.Object == nil {
+		return 0
+	}
+
 	if globals.IsVisionModel(model) {
-		// tile size is 512x512
-		// the max size of image is 2048x2048
-		// the image that is larger than 2048x2048 will be resized in 16 tiles
+		// Tile size is 512x512.
+		// Images larger than 2048x2048 are limited to 16 tiles.
+		x := LimitMax(
+			math.Ceil(float64(i.GetWidth())/512),
+			4,
+		)
+		y := LimitMax(
+			math.Ceil(float64(i.GetHeight())/512),
+			4,
+		)
 
-		x := LimitMax(math.Ceil(float64(i.GetWidth())/512), 4)
-		y := LimitMax(math.Ceil(float64(i.GetHeight())/512), 4)
 		tiles := int(x) * int(y)
-
 		return 85 + 170*tiles
 	}
 
@@ -137,87 +181,122 @@ func (i *Image) CountTokens(model string) int {
 }
 
 func (i *Image) IsBase64() bool {
-	return strings.HasPrefix(i.Content, "data:image/")
+	return i != nil &&
+		strings.HasPrefix(i.Content, "data:image/")
 }
 
 func (i *Image) GetType() string {
-	// example: image/jpeg, image/png, image/gif
+	if i == nil {
+		return ""
+	}
 
+	// Examples: image/jpeg, image/png, image/gif.
 	if i.IsBase64() {
 		t := SafeSplit(i.Content, ";", 2)[0]
 		return strings.ReplaceAll(t, "data:", "")
 	}
 
-	// example: .jpg, .png, .gif to image/jpeg, image/png, image/gif
 	switch strings.ToLower(path.Ext(i.Content)) {
 	case ".jpg", ".jpeg":
 		return "image/jpeg"
+
 	case ".png":
 		return "image/png"
+
 	case ".gif":
 		return "image/gif"
+
 	case ".webp":
 		return "image/webp"
+
 	case ".bmp":
 		return "image/bmp"
+
 	default:
 		return ""
 	}
 }
 
 func (i *Image) ToBase64() string {
+	if i == nil {
+		return ""
+	}
+
 	if i.IsBase64() {
 		return i.Content
 	}
 
-	// get url content and convert to base64
 	data, err := ConvertToBase64(i.Content)
 	if err != nil {
-		globals.Warn(fmt.Sprintf("cannot convert image to base64: %s", err.Error()))
+		globals.Warn(
+			fmt.Sprintf(
+				"cannot convert image to base64: %s",
+				err.Error(),
+			),
+		)
 		return ""
 	}
 
-	return fmt.Sprintf("data:%s;base64,%s", i.GetType(), data)
+	return fmt.Sprintf(
+		"data:%s;base64,%s",
+		i.GetType(),
+		data,
+	)
 }
 
 func (i *Image) ToRawBase64() string {
-	// example: return /9j/...
+	if i == nil {
+		return ""
+	}
+
 	if i.IsBase64() {
 		return SafeSplit(i.Content, ",", 2)[1]
 	}
 
-	// get url content and convert to base64
 	data, err := ConvertToBase64(i.Content)
 	if err != nil {
-		globals.Warn(fmt.Sprintf("cannot convert image to base64: %s", err.Error()))
+		globals.Warn(
+			fmt.Sprintf(
+				"cannot convert image to base64: %s",
+				err.Error(),
+			),
+		)
 		return ""
 	}
 
 	return data
 }
 
-func DownloadImage(url string, path string) error {
+func DownloadImage(url string, filePath string) error {
 	res, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			globals.Debug("[utils] close file error: %s (path: %s)", err.Error(), path)
+	defer func(body io.ReadCloser) {
+		closeErr := body.Close()
+		if closeErr != nil {
+			globals.Debug(
+				"[utils] close response body error: %s (path: %s)",
+				closeErr.Error(),
+				filePath,
+			)
 		}
 	}(res.Body)
 
-	file, err := os.Create(path)
+	file, err := os.Create(filePath)
 	if err != nil {
 		return err
 	}
 
 	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-			globals.Debug("[utils] close file error: %s (path: %s)", err.Error(), path)
+		closeErr := file.Close()
+		if closeErr != nil {
+			globals.Debug(
+				"[utils] close file error: %s (path: %s)",
+				closeErr.Error(),
+				filePath,
+			)
 		}
 	}(file)
 
@@ -226,16 +305,29 @@ func DownloadImage(url string, path string) error {
 }
 
 func StoreImage(url string) string {
-	if globals.AcceptImageStore {
-		hash := Md5Encrypt(url) + path.Ext(url)
-
-		if err := DownloadImage(url, fmt.Sprintf("storage/attachments/%s", hash)); err != nil {
-			globals.Warn(fmt.Sprintf("[utils] save image error: %s", err.Error()))
-			return url
-		}
-
-		return fmt.Sprintf("%s/attachments/%s", globals.NotifyUrl, hash)
+	if !globals.AcceptImageStore {
+		return url
 	}
 
-	return url
+	hash := Md5Encrypt(url) + path.Ext(url)
+	filePath := fmt.Sprintf(
+		"storage/attachments/%s",
+		hash,
+	)
+
+	if err := DownloadImage(url, filePath); err != nil {
+		globals.Warn(
+			fmt.Sprintf(
+				"[utils] save image error: %s",
+				err.Error(),
+			),
+		)
+		return url
+	}
+
+	return fmt.Sprintf(
+		"%s/attachments/%s",
+		globals.NotifyUrl,
+		hash,
+	)
 }
